9a6047dfced8df441a82474e2a1fda138c4b17c8
Oyetomi Oyelaja <torreyjh@gmail.com>
2026-08-16 17:24:22 +0200

    Lift counter state and handlers to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import ExponentTwo from "./components/ExponentTwo";
@@ -6,13 +7,21 @@ import ExponentFour from "./components/ExponentFour";
 import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
 
+function App() {
+  const [count, setCount] = useState(0);
 
-function App () {
+  const decrement = () => setCount((prevCount) => prevCount - 1);
+  const increment = () => setCount((prevCount) => prevCount + 1);
+
   return (
     <div className="App">
       <h2><em>Counter</em></h2>
-  
-      <Counter/>
+
+      <Counter
+        count={count}
+        decrement={decrement}
+        increment={increment}
+      />
 
       <br />
       <h2><em>Exponents</em></h2>
